8012be8d8c845d3580950e6bbd3e18cc571c3160
muva <muvacc@gmail.com>
2021-12-16 19:57:20 +0100

    two tests

--- tests/Feature/ProbaTest.php
+++ tests/Feature/ProbaTest.php
@@ -2,8 +2,9 @@
 
 use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+uses(LazilyRefreshDatabase::class);
 
-uses(RefreshDatabase::class);
 
 it('has home page', function () {
     $response = $this->get('/');
@@ -11,11 +12,17 @@ it('has home page', function () {
     $response->assertStatus(200);
 });
 
-it('inserts a product in the database', function ($name, $sku, $description, $price, $newName) {
+it('inserts a product in the database', function ($name, $sku, $description, $price) {
     Product::create(['name' => $name, 'sku' => $sku, 'description' => $description, 'price' => $price]);
     $this->get('/products')->assertSee($name);
-    Product::findOrFail(1)->update(['name' => $newName]);
-    $this->get('/products')->assertSee($newName);
 })->with([
-    ['gepard', 'ge123', 'Africki sprinter', 99.99, 'leopard']
+    ['gepard', 'ge123', 'Africki sprinter', 99.99]
+]);
+
+it('updates first product name', function ($name) {
+    Product::create(['name' => 'panter', 'sku' => 'pa123', 'description' => 'Panter za...', 'price' => 56]);
+    Product::findOrFail(1)->update(['name' => $name]);
+    $this->get('/products')->assertSee($name);
+})->with([
+    ['leopard']
 ]);
